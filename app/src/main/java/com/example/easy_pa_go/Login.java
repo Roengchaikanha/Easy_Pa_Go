acb828c1ddafba27f5789a9c86a907116f0f0de0
KIW <Thanapon2006gg@gmail.com>
2025-09-25 14:11:44 +0700

    Register Jaeaaaaa

--- app/src/main/java/com/example/easy_pa_go/Login.java
+++ app/src/main/java/com/example/easy_pa_go/Login.java
@@ -1,6 +1,9 @@
 package com.example.easy_pa_go;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.time.Instant;
+
 public class Login extends AppCompatActivity {
+    private Button BRegister;
+
 
 
     @Override
@@ -22,6 +29,16 @@ public class Login extends AppCompatActivity {
             return insets;
         });
         //ปุ่มไปหน้า Register
+        BRegister = findViewById(R.id.buttonRegister);
+        BRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("Register จะเอ๋!!");
+                Intent Register = new Intent(getApplicationContext(),Register.class);
+                startActivity(Register);
+            }
+        });
+
 
 
 
