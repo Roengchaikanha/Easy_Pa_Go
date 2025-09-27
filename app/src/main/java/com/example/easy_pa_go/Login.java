358a241a1fbb20beb49a02d8c924bc1f9e0452ca
KIW <Thanapon2006gg@gmail.com>
2025-09-27 12:16:36 +0700

    Loginnnn

--- app/src/main/java/com/example/easy_pa_go/Login.java
+++ app/src/main/java/com/example/easy_pa_go/Login.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,7 +19,12 @@ public class Login extends AppCompatActivity {
     //ประกาศตัวแปร ปุ่มไปหน้าลงทะเบียน
     private Button BRegister;
 
+    //ประกาศตัวแปร ปุ่ม login
+    private Button Blogin;
 
+    //login --> User,Password
+    EditText user,password;
+    Button bt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +44,24 @@ public class Login extends AppCompatActivity {
                 System.out.println("Register จะเอ๋!!");
                 Intent Register = new Intent(getApplicationContext(),Register.class);
                 startActivity(Register);
+
+        //User,Password,ButtonLogin
+        user = (EditText) findViewById(R.id.editUser);
+        password = (EditText) findViewById(R.id.editPassword);
+
+        Blogin = (Button)findViewById(R.id.buttonLogin);
+        Blogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (user.getText().toString().equals("6712247012")&&password.getText().toString().equals("1234")) {
+                    Intent Login = new Intent(getApplicationContext(), UserInterface.class);
+                    startActivity(Login);
+                }else {
+                    Toast.makeText(getApplicationContext(),"รหัสผ่านไม่ถูกต้อง",Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
             }
         });
 
