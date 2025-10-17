f6e75bdc1d8d05aafa1496daf00eb1eddca0e32f
KIW <Thanapon2006gg@gmail.com>
2025-10-18 01:11:28 +0700

    kiwComment

--- app/src/main/java/com/example/easy_pa_go/ShowData.java
+++ app/src/main/java/com/example/easy_pa_go/ShowData.java
@@ -16,7 +16,7 @@ import com.google.gson.annotations.SerializedName;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-// import คลาสที่จำเป็นสำหรับการทำงานต่างๆ
+// import คลาสที่จำเป็นสำหรับการทำงานต่าง
 public class ShowData extends AppCompatActivity {
 //ประกาศ Class ShowData
     private ImageView buildingImageView;
